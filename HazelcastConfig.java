01cc354a435479ce5b4cc39ae1430582b54e5287
DEVatAE <tonyling2021@yahoo.com>
2025-03-05 20:07:48 -0500

    Update HazelcastConfig.java

--- HazelcastConfig.java
+++ HazelcastConfig.java
@@ -1,19 +1,26 @@
-package com.example.hazelcastcache.config;
+package com.example.hazelcastCache.config;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HazelcastConfig {
 
-    @Bean
-    public Config hazelcastConfig1() {
+    @Bean(name = "hazelcastCustomConfig")
+    public Config hazelcastConfig() {
         return new Config()
-                .setClusterName("dev-cluster")
+                .setClusterName("hazelcast-cluster")
                 .addMapConfig(new MapConfig()
-                        .setName("my-cache")
-                        .setTimeToLiveSeconds(300)); // 5分钟后过期
+                        .setName("cache-map")
+                        .setTimeToLiveSeconds(30));
+    }
+
+    @Bean
+    public HazelcastInstance hazelcastInstance(Config config) {
+        return Hazelcast.newHazelcastInstance(config);
     }
 }
