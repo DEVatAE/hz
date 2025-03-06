17aac95b579d58f8e67ce24f4ec72c698f536c32
DEVatAE <tonyling2021@yahoo.com>
2025-03-05 20:08:19 -0500

    Update CacheController.java

--- CacheController.java
+++ CacheController.java
@@ -1,20 +1,25 @@
-package com.example.hazelcastcache.controller;
+package com.example.hazelcastCache.controller;
 
-import com.example.hazelcastcache.service.CacheService;
+import com.example.hazelcastCache.service.CacheService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cache")
 public class CacheController {
-
     private final CacheService cacheService;
 
     public CacheController(CacheService cacheService) {
         this.cacheService = cacheService;
     }
 
-    @GetMapping("/{key}")
-    public String getValue(@PathVariable String key) {
-        return cacheService.getCachedValue(key);
+    @PostMapping("/populate")
+    public String populate(@RequestParam String key, @RequestParam String value) {
+        cacheService.populateInfo(key, value);
+        return "Data populated in cache: " + key;
+    }
+
+    @GetMapping("/get/{key}")
+    public String get(@PathVariable String key) {
+        return cacheService.getInfo(key);
     }
 }
