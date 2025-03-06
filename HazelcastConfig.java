996c27539fa7573b780cf7c9afe6a2f4eaff2b44
DEVatAE <tonyling2021@yahoo.com>
2025-03-05 20:57:54 -0500

    Update HazelcastConfig.java

--- HazelcastConfig.java
+++ HazelcastConfig.java
@@ -1,22 +1,40 @@
 package com.example.hazelcastCache.config;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.config.TcpIpConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class HazelcastConfig {
 
     @Bean(name = "hazelcastCustomConfig")
     public Config hazelcastConfig() {
-        return new Config()
+        Config config = new Config()
                 .setClusterName("hazelcast-cluster")
                 .addMapConfig(new MapConfig()
                         .setName("cache-map")
                         .setTimeToLiveSeconds(30));
+
+        NetworkConfig networkConfig = config.getNetworkConfig();
+        JoinConfig joinConfig = networkConfig.getJoin();
+
+        joinConfig.getMulticastConfig().setEnabled(false);
+        TcpIpConfig tcpIpConfig = joinConfig.getTcpIpConfig();
+        tcpIpConfig.setEnabled(true)
+                .setMembers(Arrays.asList(
+                        "127.0.0.1:5701",
+                        "127.0.0.1:5702"
+                ));
+
+        return config;
     }
 
     @Bean
